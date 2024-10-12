5b67d4bd23cff4d5c4d80ec9dfc930351702910e
Guiphobia <dooguk3@gmail.com>
2024-10-12 10:16:13 +0900

    x

--- www/components/plugin.js
+++ www/components/plugin.js
@@ -1,2 +1,16 @@
-// This is a JavaScript file
-
+$(document).on("click", "#alerta", function(){
+    navigator.notification.alert("minha mensagem");
+});
+$(document).on("click","#confirm",function(){
+    function confirma(buttonIndex){
+        if(buttonIndex == "1"){
+            navigator.notification.alert("Escolheu a opção A");
+        }else{
+            navigator.notification.alert("Escolheu a opção B");
+        }
+    }
+    navigator.notification.confirm("Escolha A ou B", confirma, "Escolha:",['B','A']);
+});
+$(document).on("click","#beep",function(){
+    navigator.notification.beep(4000);
+});
