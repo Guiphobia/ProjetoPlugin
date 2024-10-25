0fa86fd89174f148742cc472a52b9bfd048ef560
Guiphobia <dooguk3@gmail.com>
2024-10-26 07:59:15 +0900

    TESTE

--- www/components/plugin.js
+++ www/components/plugin.js
@@ -1,16 +1,25 @@
-$(document).on("click", "#alerta", function(){
-    navigator.notification.alert("minha mensagem");
-});
-$(document).on("click","#confirm",function(){
-    function confirma(buttonIndex){
-        if(buttonIndex == "1"){
-            navigator.notification.alert("Escolheu a opção A");
-        }else{
-            navigator.notification.alert("Escolheu a opção B");
+document.addEventListener('deviceready', function() {
+    $(document).on("click", "#alerta", function() {
+        function retorno() {}
+        navigator.notification.alert("minha Mensagem", retorno, "Aviso!", "Aceito");
+    });
+
+    $(document).on("click", "#confirm", function() {
+        function confirma(buttonIndex) {
+            if (buttonIndex == 1) {
+                navigator.notification.alert("Escolheu a opção A");
+            } else {
+                navigator.notification.alert("Escolheu a opção B");
+            }
         }
-    }
-    navigator.notification.confirm("Escolha A ou B", confirma, "Escolha:",['B','A']);
-});
-$(document).on("click","#beep",function(){
-    navigator.notification.beep(4000);
-});
+        navigator.notification.confirm("Escolha A ou B", confirma, "Escolha:", ['B', 'A']);
+    });
+
+    $(document).on("click", "#beep", function() {
+        navigator.notification.beep(4);
+    });
+
+    $(document).on("click", "#vibra", function() {
+        navigator.vibrate(8000);
+    });
+}, false);
